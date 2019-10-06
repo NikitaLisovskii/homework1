003986e608c33e60c5b78d14f7a478d2294ec17a
NikitaLisovskii <welcomemts@mail.ru>
2019-10-07 02:33:59 +0300

    homework

--- src/homework2/Task4.java
+++ src/homework2/Task4.java
@@ -5,8 +5,8 @@ public class Task4 {
        Ram  ram = new Ram(16);
        Hdd hdd = new Hdd(256);
        Computer computer = new Computer(ram,hdd);
-       System.out.println(computer instanceof Computer);  // возвращает true - объект класса Computer
-       System.out.println(computer instanceof IComputer);  // возвращает  true - объект класса IComputer
+       System.out.println(computer instanceof Computer);
+       System.out.println(computer instanceof IComputer);
    }
 
 }
